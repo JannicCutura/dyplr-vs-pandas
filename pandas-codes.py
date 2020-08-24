6ced43ef6b0950d12f5d243f239841a83c5c38d6
Jannic Cutura <Janniccutura@gmail.com>
2020-08-24 14:58:26 +0200

    feat: added csnap

--- pandas-codes.py
+++ pandas-codes.py
@@ -4,24 +4,44 @@ import pandas as pd
 from nycflights13 import flights
 print("This is pandas version {}. Make sure your version is >0.25 ".format(pd.__version__))
 
-
+def csnap(df, fn=lambda x: x.shape, msg=None):
+    """ Custom Help function to print things in method chaining.
+        Returns back the df to further use in chaining.
+        Credits: https://towardsdatascience.com/the-unreasonable-effectiveness-of-method-chaining-in-pandas-15c2109e3c69
+    """
+    if msg:
+        print(msg)
+    display(fn(df))
+    return df
 
 
 df = flights
 
+df.head()
 
-df['avg_speed'] = df['distance'] / df['air_time']
-df['LongFlight'] = df['air_time'] > 150.6865
+df.columns
 
 
-df = df.assign(avg_speed = lambda x: x['distance']/ x['air_time']) \
+df = flights
+
+df = df.pipe(csnap,  msg="Let's start") \
+       .assign(avg_speed = lambda x: x['distance']/ x['air_time']) \
+       .assign(LongFlight = lambda x: x['air_time'] > 15.6865) \
+       .pipe(csnap, lambda x: x.head(), msg="Added two columns")
+
+
+
         .query('air_time > 15.6865')
 
 #change date format
 #.assign(QTR = lambda x: x.QTR.dt.to_period('Q'))
-df.groupby(['origin','day']).agg(
-    max_airtime=pd.NamedAgg(column='air_time',aggfunc='max'),
-    mean_airtime=pd.NamedAgg('air_time','mean')).reset_index()
+df.groupby(['origin','day'])\
+  .agg(
+    airtime_avg=pd.NamedAgg(column='air_time',aggfunc='mean'),
+    airtime_max=pd.NamedAgg(column='air_time',aggfunc='max'),
+    airtime_min=pd.NamedAgg(column='air_time',aggfunc='min'),
+    airtime_std=pd.NamedAgg(column='air_time',aggfunc='std'))\
+  .reset_index()
 
 
 
