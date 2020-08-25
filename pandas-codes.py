65503f95cc5874382dfa93acdc5669647f798ad8
Jannic Cutura <Janniccutura@gmail.com>
2020-08-25 16:05:50 +0200

    feat: added reorder function and some filtering

--- pandas-codes.py
+++ pandas-codes.py
@@ -1,11 +1,12 @@
-
 ## import libraries
 import numpy as np
 import pandas as pd
 import time
 from nycflights13 import flights
+
 print("This is pandas version {}. Make sure your version is >0.25 ".format(pd.__version__))
 
+## useful functions
 def csnap(df, fn=lambda x: x.shape, msg=None):
     """ Custom Help function to print things in method chaining.
         Returns back the df to further use in chaining.
@@ -17,95 +18,105 @@ def csnap(df, fn=lambda x: x.shape, msg=None):
     return df
 
 
+def reorder(df, cols):
+    """Reorders the dataframe df by taking the provided columns first and then
+    puts the remaining columns
+
+    Parameters:
+        df (panda dataframe): A panda dataframe which columns should be reordered
+        cols (list): a list vontaining the names of the columns that should be first
+        """
+    others = df.columns.difference(cols).tolist()
+    return df[cols+others]
+
+
+
+
 df = flights
 
-df.head()
 
+# some stats and info
+df.head()
 df.columns
 
 
-df = flights
-
-df = df.pipe(csnap,  msg="Let's start") \
-       .assign(avg_speed = lambda x: x['distance']/ x['air_time']) \
-       .assign(LongFlight = lambda x: x['air_time'] > 15.6865) \
-       .pipe(csnap, lambda x: x.head(), msg="Added two columns")
 
 
+## add / replace variables
+df = df.assign(sched_arr_time2 =lambda x:  x.sched_arr_time**2,
+               dep_time=lambda x: np.where(x.arr_delay < 0, -99, x.dep_time),
+               avg_speed=lambda x: x['distance'] / x['air_time'],
+               LongFlight=lambda x: x['air_time'] > 15.6865)
+
+## order variables
+df = df.pipe(reorder, ['year', 'month', 'day','dep_delay'])
+
+## rename columns
+df = df.rename({'time_hour': 'timehour'}, axis='columns')
+
+
+# groupy by summarise
+df.groupby(['origin', 'day']) \
+    .agg(
+    airtime_avg=pd.NamedAgg(column='air_time', aggfunc='mean'),
+    airtime_max=pd.NamedAgg(column='air_time', aggfunc='max'),
+    airtime_min=pd.NamedAgg(column='air_time', aggfunc='min'),
+    airtime_std=pd.NamedAgg(column='air_time', aggfunc='std')) \
+    .reset_index()
 
 
 
-#change date format
-#.assign(QTR = lambda x: x.QTR.dt.to_period('Q'))
-df.groupby(['origin','day'])\
-  .agg(
-    airtime_avg=pd.NamedAgg(column='air_time',aggfunc='mean'),
-    airtime_max=pd.NamedAgg(column='air_time',aggfunc='max'),
-    airtime_min=pd.NamedAgg(column='air_time',aggfunc='min'),
-    airtime_std=pd.NamedAgg(column='air_time',aggfunc='std'))\
-  .reset_index()
-
-df['test'] =1
-df.loc[df.index[2],'test'] = 50
+## groupby mutate
 df = df.query('air_time > 15.6865') \
-       .pipe(csnap,  msg="Before transform") \
-       .join(df.groupby(['origin','day'])
-               .agg(airtime_avg=pd.NamedAgg(column='test',aggfunc='mean'),
-                    airtime_max=pd.NamedAgg(column='air_time',aggfunc='max'),
-                    airtime_min=pd.NamedAgg(column='air_time',aggfunc='min'),
-                    airtime_std=pd.NamedAgg(column='air_time',aggfunc='std')),on=['origin','day'], how="left")\
-       .pipe(csnap,  msg = "After transform")
+    .pipe(csnap, msg="Before transform") \
+    .join(df.groupby(['origin', 'day'])
+          .agg(airtime_avg=pd.NamedAgg(column='air_time', aggfunc='mean'),
+               airtime_max=pd.NamedAgg(column='air_time', aggfunc='max'),
+               airtime_min=pd.NamedAgg(column='air_time', aggfunc='min'),
+               airtime_std=pd.NamedAgg(column='air_time', aggfunc='std'),
+               count_connections=pd.NamedAgg(column='air_time', aggfunc='count'),
+               count_connections2=pd.NamedAgg(column='air_time', aggfunc='size')
+               ), on=['origin', 'day'], how="left") \
+    .pipe(csnap, msg="After transform")
 
 
+
+
+## filter
+df4 = df.query("dep_delay > 0 and arr_delay.notnull() and carrier.str.contains('U') and tailnum.str.len() == 5", engine='python')
+
+
+## working with dates and time
+
+
+
+
+
+
+
+## other
 import pandas as pd
 import numpy as np
+
 # Import CSV mtcars
-data = pd.read_csv('https://gist.githubusercontent.com/ZeccaLehn/4e06d2575eb9589dbe8c365d61cb056c/raw/64f1660f38ef523b2a1a13be77b002b98665cdfe/mtcars.csv') \
-         .rename(columns={'Unnamed: 0':'brand'}) \
-         .assign(mpg=lambda x: np.where(x.cyl == 4, -99, x.mpg))
+data = pd.read_csv(
+    'https://gist.githubusercontent.com/ZeccaLehn/4e06d2575eb9589dbe8c365d61cb056c/raw/64f1660f38ef523b2a1a13be77b002b98665cdfe/mtcars.csv') \
+    .rename(columns={'Unnamed: 0': 'brand'}) \
+    .assign(mpg=lambda x: np.where(x.cyl == 4, -99, x.mpg))
 
-
-
-
-
-df = pd.DataFrame(np.random.randint(0,100,size=(50000000, 2)), columns=list('AB'))
+df = pd.DataFrame(np.random.randint(0, 100, size=(50000000, 2)), columns=list('AB'))
 start_time = time.time()
-df2 = df.loc[df.B <50, 'A' ] = -99
+df2 = df.loc[df.B < 50, 'A'] = -99
 print("--- %s seconds ---" % (time.time() - start_time))
 
-
 start_time = time.time()
-df3= df.assign(A=lambda x: np.where(x.B < 50, -99, x.A))
+df3 = df.assign(A=lambda x: np.where(x.B < 50, -99, x.A))
 print("--- %s seconds ---" % (time.time() - start_time))
-
-
-
-
-
-df['mean_airtime'] = df.groupby(['origin','day'], as_index=True)['air_time'].transform('mean')
-df['min_airtime'] = df.groupby(['origin','day'], as_index=True)['air_time'].transform('min')
-df['max_airtime'] = df.groupby(['origin','day'], as_index=True)['air_time'].transform('max')
-df['std_airtime'] = df.groupby(['origin','day'], as_index=True)['air_time'].transform('std')
-
-# count number of times a connection was used
-df['count_connections'] = df.groupby(['origin','dest'], as_index=True)['day'].transform('size')
-
-# the same, but don't count if day is NA.
-df['count_connections2'] = df.groupby(['origin','dest'], as_index=True)['day'].transform('count')
-
-
-
-
 
 
 
 df[df.dep_delay > 150.6865]
 df[df["dep_delay"] > 150.6865]
-
-
-
-
-
 
 
 
