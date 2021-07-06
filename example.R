257b93738004c60db0246c2b650ed6bf7bb13898
Jannic Cutura <Janniccutura@gmail.com>
2021-07-06 11:58:11 +0200

    Add files via upload

---
+++ example.R
@@ -0,0 +1,6 @@
+
+word <- gsub("=gesellschaftmitbeschränkterhaftung=","",fixed = T,x = word)
+word <- gsub("landesbankbadenwãfârttemberginstitut","landesbankbadenwüttemberginstitut",fixed = T,x = word) 
+word <- gsub("kreditanstaltfãrwiederaufbau","kreditanstaltfãrwiederaufbau",fixed = T,x = word) 
+
+ 
